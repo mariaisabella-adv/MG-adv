d8713e43215a7247dc9a84ba03f888042516bd06
Eduardo Vieira <evieira142@gmail.com>
2026-02-09 15:02:43 -0300

    atualizações

--- js/main.js
+++ js/main.js
@@ -16,45 +16,44 @@ navLinks.forEach((link) => {
 });
 
 // Header com efeito no scroll
+const header = document.querySelector("header");
 
-const header = document.querySelector('header')
-
-window.addEventListener('scroll', () => {
+window.addEventListener("scroll", () => {
   if (window.scrollY > 80) {
-    header.classList.add('scrolled')
+    header.classList.add("scrolled");
   } else {
-    header.classList.remove('scrolled')
+    header.classList.remove("scrolled");
   }
-})
+});
 
 // Scroll Suave
 
-document.querySelectorAll('a[href^="#"]').forEach(link => {
-  link.addEventListener('click', function (e) {
-    e.preventDefault()
+document.querySelectorAll('a[href^="#"]').forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
 
-    const target = document.querySelector(this.getAttribute('href'))
-    if (!target) return
+    const target = document.querySelector(this.getAttribute("href"));
+    if (!target) return;
 
     target.scrollIntoView({
       behavior: "smooth",
-      block: "start"
-    })
-  })
-})
+      block: "start",
+    });
+  });
+});
 
 // Whatsapp Integração
 
-const whatsappButtons = document.querySelectorAll('.whatsapp-btn')
+const whatsappButtons = document.querySelectorAll(".whatsapp-btn");
 
 const phone = "5592981088441";
 const message = encodeURIComponent(
-  "Olá, Dra. Maria Isabella. Gostaria de obter uma orientação jurídica."
-)
+  "Olá, Dra. Maria Isabella. Gostaria de obter uma orientação jurídica.",
+);
 
-whatsappButtons.forEach(btn => {
-  btn.addEventListener('click', (e) => {
-    e.preventDefault()
-    window.open(`https://wa.me/${phone}?text=${message}`, "_blank")
-  })
-})
+whatsappButtons.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.open(`https://wa.me/${phone}?text=${message}`, "_blank");
+  });
+});
